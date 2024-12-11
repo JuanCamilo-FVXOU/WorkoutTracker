aefd172ce7823ddb7a0482abaac5b92cd3149802
santiagomac <smazuerac@unac.edu.co>
2024-12-11 17:26:59 -0500

    change dependency from database

--- src/main/java/com/workout_tracker/domain/usecases/CreateWorkoutUseCase.java
+++ src/main/java/com/workout_tracker/domain/usecases/CreateWorkoutUseCase.java
@@ -1,12 +1,15 @@
 package com.workout_tracker.domain.usecases;
 
 import com.workout_tracker.infrastructure.driven_adapter.postgres.entity.Workout;
+import com.workout_tracker.infrastructure.driven_adapter.postgres.repository.WorkoutRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class CreateWorkoutUseCase {
 
-  public void createWorkout(Workout workout) {
+  private final WorkoutRepository workoutRepository;
 
+  public void createWorkout(Workout workout) {
+    this.workoutRepository.save(workout);
   }
 }
